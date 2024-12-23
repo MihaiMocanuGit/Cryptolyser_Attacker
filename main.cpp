c2aaa1219093559b90a22b054dd59225ee77c099
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2024-12-23 02:10:47 +0200

    Data is now saved to a csv file.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include "ServerConnection/ServerConnection.hpp"
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
+    constexpr bool PRINT_TO_STD = false;
+    if (argc != 4)
     {
-        std::cerr << "Incorrect program parameters: <IPv4> <PORT>" << std::endl;
+        std::cerr << "Incorrect program parameters: <IPv4> <PORT> <CSV_FILE_PATH>" << std::endl;
         return -1;
     }
-    const std::string_view ip = argv[1];
-    const uint16_t port = std::stoi(argv[2]);
+
+    const std::string_view ip{argv[1]};
+    const uint16_t port{static_cast<uint16_t>(std::stoi(argv[2]))};
+    std::ofstream csvFile;
+    csvFile.open(argv[3]);
 
     ServerConnection connection{ip, port};
-    std::cout << "Count, InboundSec, InboundNanoSec, OutboundSec, OutboundNanoSec\n";
-    for (size_t count = 0;;)
+    const std::string header{
+        "Count, Size, InboundSec, InboundNanoSec, OutboundSec, OutboundNanoSec\n"};
+    if constexpr (PRINT_TO_STD)
+        std::cout << header;
+    csvFile << header;
+    for (size_t count{0};;)
     {
         for (size_t dataSize = 1; dataSize <= 500; ++dataSize, count++)
         {
@@ -28,18 +37,27 @@ int main(int argc, char **argv)
                                   static uint8_t value = 0;
                                   return static_cast<std::byte>(value++);
                               });
-                auto result = connection.transmit(data);
+                auto result{connection.transmit(data)};
                 if (result)
                 {
-                    std::cout << count << ", " << dataSize << ", " << result->inbound_sec << ", "
-                              << result->inbound_nsec << ", " << result->outbound_sec << ", "
-                              << result->outbound_nsec << '\n';
+                    const std::string outputRow{std::to_string(count) + ", " +
+                                                std::to_string(dataSize) + ", " +
+                                                std::to_string(result->inbound_sec) + ", " +
+                                                std::to_string(result->inbound_nsec) + ", " +
+                                                std::to_string(result->outbound_sec) + ", " +
+                                                std::to_string(result->outbound_nsec) + '\n'};
+                    if constexpr (PRINT_TO_STD)
+                        std::cout << outputRow;
+                    csvFile << outputRow;
                 }
                 connection.closeConnection();
             }
             else
             {
                 std::cerr << "Could not connect." << std::endl;
+                const std::string outputRow{std::to_string(count) + ", " +
+                                            std::to_string(dataSize) + ", -1, -1, -1, -1\n"};
+                csvFile << outputRow;
             }
         }
     }
