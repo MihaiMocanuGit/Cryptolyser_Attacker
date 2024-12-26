bc685c7d6d33d1145b2e7023d712bf06d29793ce
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2024-12-26 23:28:44 +0200

    Program now safely exits on SIGTERM, SIGINT and SIGQUIT.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "ServerConnection/ServerConnection.hpp"
 
 #include <algorithm>
+#include <csignal>
 #include <fstream>
 #include <iostream>
+
+namespace
+{
+volatile sig_atomic_t g_continueRunning = true;
+void exitHandler(int signal) { g_continueRunning = false; }
+} // namespace
 
 int main(int argc, char **argv)
 {
@@ -25,47 +32,57 @@ int main(int argc, char **argv)
         std::cout << header;
     csvFile << header;
     size_t lostPackages = 0;
-    for (size_t count{0};;)
+
+    struct sigaction sigactionExit{};
+    sigactionExit.sa_handler = exitHandler;
+    sigemptyset(&sigactionExit.sa_mask);
+    sigactionExit.sa_flags = SA_RESTART;
+    for (auto sig : {SIGTERM, SIGINT, SIGQUIT})
     {
-        for (size_t dataSize = 1; dataSize <= 500; ++dataSize, count++)
+        if (sigaction(sig, &sigactionExit, nullptr))
         {
-            if (connection.connect())
+            std::perror("Error setting signal handler\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    for (size_t dataSize{1}, count{0}; g_continueRunning; dataSize = dataSize % 500 + 1, count++)
+    {
+        if (connection.connect())
+        {
+            std::vector<std::byte> data{dataSize};
+            std::generate(data.begin(), data.end(),
+                          []()
+                          {
+                              static uint8_t value = 0;
+                              return static_cast<std::byte>(value++);
+                          });
+            auto result{connection.transmit(data)};
+            if (result)
             {
-                std::vector<std::byte> data{dataSize};
-                std::generate(data.begin(), data.end(),
-                              []()
-                              {
-                                  static uint8_t value = 0;
-                                  return static_cast<std::byte>(value++);
-                              });
-                auto result{connection.transmit(data)};
-                if (result)
-                {
-                    const std::string outputRow{std::to_string(count) + ", " +
-                                                std::to_string(dataSize) + ", " +
-                                                std::to_string(result->inbound_sec) + ", " +
-                                                std::to_string(result->inbound_nsec) + ", " +
-                                                std::to_string(result->outbound_sec) + ", " +
-                                                std::to_string(result->outbound_nsec) + '\n'};
-                    if constexpr (PRINT_TO_STD)
-                        std::cout << outputRow;
-                    csvFile << outputRow;
-                }
-                else
-                {
-                    std::cout << "Loss ratio: "
-                              << static_cast<long double>(++lostPackages) / (count + 1)
-                              << std::endl;
-                    const std::string outputRow{std::to_string(count) + ", " +
-                                                std::to_string(dataSize) + ", -1, -1, -1, -1\n"};
-                    csvFile << outputRow;
-                }
-                connection.closeConnection();
+                const std::string outputRow{std::to_string(count) + ", " +
+                                            std::to_string(dataSize) + ", " +
+                                            std::to_string(result->inbound_sec) + ", " +
+                                            std::to_string(result->inbound_nsec) + ", " +
+                                            std::to_string(result->outbound_sec) + ", " +
+                                            std::to_string(result->outbound_nsec) + '\n'};
+                if constexpr (PRINT_TO_STD)
+                    std::cout << outputRow;
+                csvFile << outputRow;
             }
-            else
+            else if (g_continueRunning)
             {
-                std::cerr << "Could not connect." << std::endl;
+                std::cout << "Loss ratio: "
+                          << static_cast<long double>(++lostPackages) / (count + 1) << std::endl;
+                const std::string outputRow{std::to_string(count) + ", " +
+                                            std::to_string(dataSize) + ", -1, -1, -1, -1\n"};
+                csvFile << outputRow;
             }
+            connection.closeConnection();
+        }
+        else
+        {
+            std::cerr << "Could not connect." << std::endl;
         }
     }
     return 0;
