87d9f7dc09ed2961938b22410023bf07a74f1d17
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-03-05 20:36:03 +0200

    Timer | Added assert for the validity of uint to Real_t conversion.

--- src/DataProcessing/TimingProcessing.hpp
+++ src/DataProcessing/TimingProcessing.hpp
@@ -1,7 +1,9 @@
 #ifndef CRYPTOLYSER_ATTACKER_TIMINGPROCESSING_HPP
 #define CRYPTOLYSER_ATTACKER_TIMINGPROCESSING_HPP
 
+#include <cassert>
 #include <cstdint>
+
 namespace TimingProcessing
 {
 template <typename Real_t>
@@ -11,6 +13,8 @@ Real_t computeDT(uint64_t inbound_t1, uint64_t inbound_t2, uint64_t outbound_t1,
     const uint64_t dT1 = outbound_t1 - inbound_t1;
     const uint64_t dT2 = outbound_t2 - inbound_t2;
     uint64_t uintDt = 1'000'000'000 * dT1 + dT2;
+    auto converted = static_cast<Real_t>(uintDt);
+    assert(converted == uintDt);
     return static_cast<Real_t>(uintDt);
 }
 
