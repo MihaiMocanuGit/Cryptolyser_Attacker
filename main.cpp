4e2b9a78bc5ef160d4959c2e39ddfb767e3b3b5e
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-03-15 15:03:42 +0200

    Doppelganger | The main format used in experimenting.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "Correlate/Correlate.hpp"
 #include "DataProcessing/Samples/SampleGroup.hpp"
 #include "DataProcessing/Timings/TimingProcessing.hpp"
 #include "ServerConnection/ServerConnection.hpp"
@@ -46,7 +47,9 @@ int main(int argc, char **argv)
 
     const std::string_view ip{argv[1]};
     const uint16_t port{static_cast<uint16_t>(std::stoi(argv[2]))};
-    ServerKeyConnection connection{ip, port};
+    ServerKeyConnection connection1{ip, port};
+    ServerKeyConnection connection2{ip, port};
+    ServerKeyConnection connection3{ip, port};
 
     struct sigaction sigactionExit{};
     sigactionExit.sa_handler = exitHandler;
@@ -62,21 +65,35 @@ int main(int argc, char **argv)
     }
 
     constexpr size_t dataPacketLength = 512;
-    constexpr size_t desiredAvgSampleSize = 4048 * 4;
-    const StudyDoppelganger::DisplayParams display{
-        .printFreq = 200'000,
-        .saveFreq = 2'000'000,
-        .savePath = saveFolderPath,
-    };
+    StudyDoppelganger study1{std::move(connection1), g_continueRunning, dataPacketLength};
+    study1.loadRawStudyData("../../DATA/REMOTE_NEW_KEY_1/Raw");
 
-    StudyDoppelganger study{std::move(connection), g_continueRunning, dataPacketLength};
-    std::cout << "Calibrating bounds..." << std::endl;
-    StudyDoppelganger::TimingBoundaryParams computedBd = study.calibrate(saveFolderPath);
-    std::cout << "Computed bounds: " << computedBd.lb << ", " << computedBd.ub << std::endl;
-    std::cout << "Starting study..." << std::endl;
+    StudyDoppelganger study2{std::move(connection2), g_continueRunning, dataPacketLength};
+    study2.loadRawStudyData("../../DATA/REMOTE_OLD_KEY_1/Raw");
 
-    study.start(desiredAvgSampleSize, display, computedBd);
+    StudyDoppelganger study3{std::move(connection2), g_continueRunning, dataPacketLength};
+    study3.loadRawStudyData("../../DATA/REMOTE_NEW_KEY_2/Raw");
 
-    std::cout << "Exiting..." << std::endl;
+    std::cout << "Different Key:\n";
+    for (unsigned i{0}; i < Study::AES_BLOCK_SIZE; ++i)
+    {
+        double result1 = Correlate::computeFactorStdSampleGroup(study1.data()[i], study2.data()[i]);
+        double result2 = Correlate::computeFactorSampleGroup(study1.data()[i], study2.data()[i]);
+        std::cout << i << ":\t" << result1 << '\t' << result2 << '\n';
+    }
+    std::cout << "Final factor:\t"
+              << Correlate::computeFactorStdSampleBlock(study1.data(), study2.data()) << '\t'
+              << Correlate::computeFactorSampleBlock(study1.data(), study2.data()) << "\n\n";
+
+    std::cout << "Same Key:\n";
+    for (unsigned i{0}; i < Study::AES_BLOCK_SIZE; ++i)
+    {
+        double result1 = Correlate::computeFactorStdSampleGroup(study1.data()[i], study3.data()[i]);
+        double result2 = Correlate::computeFactorSampleGroup(study1.data()[i], study3.data()[i]);
+        std::cout << i << ":\t" << result1 << '\t' << result2 << '\n';
+    }
+    std::cout << "Final factor:\t"
+              << Correlate::computeFactorStdSampleBlock(study1.data(), study3.data()) << '\t'
+              << Correlate::computeFactorSampleBlock(study1.data(), study3.data()) << "\n\n";
     return 0;
 }
