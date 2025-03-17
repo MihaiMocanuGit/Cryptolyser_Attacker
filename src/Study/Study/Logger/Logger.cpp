13c8f311f4b59337e19a8c5c59d7107d5f4f208e
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-03-17 23:41:19 +0200

    New Study | Defined Logger class.

--- src/Study/Study/Logger/Logger.cpp
+++ src/Study/Study/Logger/Logger.cpp
@@ -1,1 +1,123 @@
 #include "Logger.hpp"
+
+#include <iomanip>
+
+template <bool KnownKey>
+void Logger<KnownKey>::printStats()
+{
+    const double completionPercent{static_cast<double>(m_totalValidCount()) /
+                                   static_cast<double>(m_finalValidCount) * 100.0};
+    const double invalidPercent{100.0 - static_cast<double>(m_totalValidCount()) /
+                                            static_cast<double>(m_totalSentCount()) * 100.0};
+
+    timespec currentTime{};
+    clock_gettime(CLOCK_MONOTONIC, &currentTime);
+    double totalElapsedTime{
+        TimingProcessing::computeDT<double>(m_startStudyTime.tv_sec, m_startStudyTime.tv_nsec,
+                                            currentTime.tv_sec, currentTime.tv_nsec)};
+    totalElapsedTime *= 1.0e-9; // nanosec to sec
+    // Estimated time until completion in minutes
+    const double ETA = (100.0 - completionPercent) * static_cast<double>(totalElapsedTime) /
+                       (completionPercent * 60.0);
+
+    double passTime{TimingProcessing::computeDT<double>(
+        m_prevPassTime.tv_sec, m_prevPassTime.tv_nsec, currentTime.tv_sec, currentTime.tv_nsec)};
+    passTime *= 1.0e-9; // nanosec to sec
+    const double rate{static_cast<double>(m_gatherer.validValuesCount() - m_prevPassPacketCount) /
+                      passTime};
+
+    m_prevPassTime = currentTime;
+    m_prevPassPacketCount = m_gatherer.validValuesCount();
+    // TODO: convert to fixed width columns
+    std::cout << "Stats:\n"                                                         //
+              << "\tETA: " << ETA << " minutes"                                     //
+              << "\t Progress: " << m_totalValidCount() << '/' << m_finalValidCount //
+              << " (" << completionPercent << "%)"                                  //
+              << "\t Study Rate: " << rate << " packets/second"                     //
+              << "\t Invalid count: " << m_totalSentCount() - m_totalValidCount()   //
+              << " (" << invalidPercent << "%)"                                     //
+              << '\n';                                                              //
+
+    const auto &blockTimings = m_gatherer.timingData().blockTimings;
+    const size_t avgSampleSize{m_totalValidCount() / 256};
+    double min = blockTimings[0].globalMetrics().min;
+    double max = blockTimings[0].globalMetrics().max;
+    for (const auto &sampleGroup : blockTimings)
+    {
+        min = std::min(min, sampleGroup.globalMetrics().min);
+        max = std::max(max, sampleGroup.globalMetrics().max);
+    }
+
+    std::cout << "Sample Group:" << '\n'; //
+    if (m_totalValidCount() == 0)
+        std::cout << "\tEmpty.\n";
+    else
+    {
+        std::cout << std::fixed << std::setprecision(3)                     //
+                  << "\tMean: " << blockTimings[0].globalMetrics().mean     //
+                  << "\tStdDev: " << blockTimings[0].globalMetrics().stdDev //
+                  << "\tAverage Sample Size: " << avgSampleSize             //
+                  << "\tMin: " << min                                       //
+                  << "\tMax: " << max                                       //
+                  << '\n';                                                  //
+    }
+
+    auto printIgnoredBound = [this](double bound, const SampleData<double> &sample)
+    {
+        float ratio = static_cast<float>(sample.metrics().size) / m_totalReceivedCount() * 100.0;
+        std::cout << "Values for bound: " << bound << '\n';
+        if (sample.metrics().size == 0)
+            std::cout << "\tEmpty.\n";
+        else
+        {
+            std::cout << std::fixed << std::setprecision(3)                           //
+                      << "\tMean: " << sample.metrics().mean                          //
+                      << "\tStdDev: " << sample.metrics().stdDev                      //
+                      << "\tSize: " << sample.metrics().size << " (" << ratio << "%)" //
+                      << "\tMin: " << sample.metrics().min                            //
+                      << "\tMax: " << sample.metrics().max                            //
+                      << '\n';                                                        //
+        }
+    };
+
+    printIgnoredBound(m_gatherer.lb(), m_gatherer.sampleLB());
+    printIgnoredBound(m_gatherer.ub(), m_gatherer.sampleUB());
+
+    std::cout << '\n';
+}
+
+template <bool KnownKey>
+void Logger<KnownKey>::init(size_t finalValidValuesCount)
+{
+    m_finalValidCount = finalValidValuesCount;
+
+    clock_gettime(CLOCK_MONOTONIC, &m_startStudyTime);
+    m_prevPassTime = m_startStudyTime;
+}
+
+template <bool KnownKey>
+Logger<KnownKey>::Logger(const Gatherer<KnownKey> &gatherer) noexcept : m_gatherer{gatherer}
+{
+}
+
+template <bool KnownKey>
+size_t Logger<KnownKey>::m_totalValidCount() const
+{
+    return m_gatherer.validValuesCount();
+}
+
+template <bool KnownKey>
+size_t Logger<KnownKey>::m_totalReceivedCount() const
+{
+    return m_totalValidCount() + m_gatherer.sampleLB().data().size() +
+           m_gatherer.sampleUB().data().size();
+}
+
+template <bool KnownKey>
+size_t Logger<KnownKey>::m_totalSentCount() const
+{
+    return m_totalReceivedCount() + m_gatherer.lostPackages();
+}
+
+template class Logger<true>;
+template class Logger<false>;
--- src/Study/Study/Logger/Logger.hpp
+++ src/Study/Study/Logger/Logger.hpp
@@ -1,8 +1,26 @@
 #ifndef CRYPTOLYSER_ATTACKER_LOGGER_HPP
 #define CRYPTOLYSER_ATTACKER_LOGGER_HPP
 
+#include "DataProcessing/Timings/TimingProcessing.hpp"
+#include "Study/Gatherer/Gatherer.hpp"
+
+template <bool KnownKey>
 class Logger
 {
+  private:
+    const Gatherer<KnownKey> &m_gatherer;
+    timespec m_startStudyTime{};
+    timespec m_prevPassTime{.tv_sec = 0, .tv_nsec = 0};
+    size_t m_prevPassPacketCount{0};
+    size_t m_finalValidCount;
+    [[nodiscard]] size_t m_totalSentCount() const;
+    [[nodiscard]] size_t m_totalReceivedCount() const;
+    [[nodiscard]] size_t m_totalValidCount() const;
+
+  public:
+    explicit Logger(const Gatherer<KnownKey> &gatherer) noexcept;
+    void init(size_t finalValidValuesCount);
+    void printStats();
 };
 
 #endif // CRYPTOLYSER_ATTACKER_LOGGER_HPP
