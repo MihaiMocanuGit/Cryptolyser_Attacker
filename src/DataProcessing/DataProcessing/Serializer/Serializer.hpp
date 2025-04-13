db4121904da5d950e2602da3ee26216f53c315a3
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-04-14 01:36:33 +0300

    Study Refactor | Fixed the linking logic for serializer.

--- src/DataProcessing/DataProcessing/Serializer/Serializer.hpp
+++ src/DataProcessing/DataProcessing/Serializer/Serializer.hpp
@@ -1,52 +1,20 @@
 #ifndef CRYPTOLYSER_ATTACKER_SERIALIZER_HPP
 #define CRYPTOLYSER_ATTACKER_SERIALIZER_HPP
 
-#include "../DataVector/DataVector.hpp"
-#include "../DataVector/DataVectorSerializer.hpp"
-#include "../DistributionData/DistributionData.hpp"
-#include "../Metrics/Metrics.hpp"
+/// The order of includes is important
+/// For example, DataVector must know about SampleData and MetricsData as a vector can contain both
+/// of them
+
+/////////////////////////////////////////////////////////////
 #include "../Metrics/MetricsSerializer.hpp"
-#include "../MetricsData/MetricsData.hpp"
+/////////////////////////////////////////////////////////////
 #include "../MetricsData/MetricsDataSerializer.hpp"
-#include "../SampleData/SampleData.hpp"
+/////////////////////////////////////////////////////////////
 #include "../SampleData/SampleDataSerializer.hpp"
+/////////////////////////////////////////////////////////////
+#include "../DataVector/DataVectorSerializer.hpp"
+/////////////////////////////////////////////////////////////
+#include "../DistributionData/DistributionDataSerializer.hpp"
+/////////////////////////////////////////////////////////////
 
-#include <filesystem>
-
-// While these function declarations are not needed for linkage/compiling, I've decided to include
-// them to clarify the API structure.
-namespace Serializer
-{
-template <Real T>
-extern void saveToCsv(const std::filesystem::path &path, const Metrics<T> &metrics);
-
-template <Real T>
-extern void loadFromCsv(const std::filesystem::path &path, Metrics<T> &metrics);
-
-template <Real T>
-extern void saveToCsv(const std::filesystem::path &path, const MetricsData<T> &metricsData);
-
-template <Real T>
-extern void loadFromCsv(const std::filesystem::path &path, MetricsData<T> &metricsData);
-
-template <Real T>
-extern void saveToCsv(const std::filesystem::path &path, const New::SampleData<T> &sampleData);
-
-template <Real T>
-extern void loadFromCsv(const std::filesystem::path &path, New::SampleData<T> &sampleData);
-
-template <Real T>
-extern void saveToCsv(const std::filesystem::path &path,
-                      const DistributionData<T> &distributionData);
-
-template <Real T>
-extern void loadFromCsv(const std::filesystem::path &path, DistributionData<T> &distributionData);
-
-template <HasMetric T>
-extern void saveToCsv(const std::filesystem::path &path, const DataVector<T> &metricsVector);
-
-template <HasMetric T>
-extern void loadFromCsv(const std::filesystem::path &path, DataVector<T> &metricsVector);
-
-} // namespace Serializer
 #endif // CRYPTOLYSER_ATTACKER_SERIALIZER_HPP
