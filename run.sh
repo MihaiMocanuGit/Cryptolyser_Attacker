1f0a9ec1379f0728ad840da16920891e70c73358
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-04-30 00:21:59 +0300

    Misc | Building on 16 threads

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ build_type=$1
 echo "build_type = $build_type"
 
 echo "Setting build options"
-time (cmake -DCMAKE_BUILD_TYPE="$build_type" -S . -B build-"$build_type" && echo "Building $build_type" && cmake --build build-"$build_type")
+time (cmake -DCMAKE_BUILD_TYPE="$build_type" -S . -B build-"$build_type" && echo "Building $build_type" && cmake --build build-"$build_type" -j 16)
 
 echo "Running"
 time (build-"$build_type"/Cryptolyser_Attacker "$2" "$3" "$4")
